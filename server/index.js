35c544754b7217490eb80f29074e0bb38e45daba
Nikita Babko <niki-babko@yandex.ru>
2021-02-23 16:03:23 +0700

    Added connect to mongoDB.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const app = express();
-
-app.get('/auth', (req, res) => {
-  res.send({
-    name: 'Nikita',
-  });
-});
+dotenv.config();
 
 // Server
+const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) => {
   if (err) {
@@ -16,3 +13,16 @@ app.listen(PORT, (err) => {
   }
 });
 console.log(`Server has started on port: ${PORT}`);
+
+// Connect ot mongoDB
+mongoose.connect(
+  process.env.MDB_CONNETC,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  (err) => {
+    if (err) throw Error(err);
+    console.log('Connected to mongoDB');
+  }
+);
