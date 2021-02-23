0f123204291a3a59a0a9dcfc53f2e013e79c4ae8
Nikita Babko <niki-babko@yandex.ru>
2021-02-23 17:48:25 +0700

    Added hashing of password and jwt token.

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/', async (req, res) => {
   try {
@@ -30,6 +32,35 @@ router.post('/', async (req, res) => {
         errorMessage: 'An account with this email already exists.',
       });
     }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Save a new user account to the DB
+    const newUser = new User({
+      email,
+      passwordHash,
+    });
+
+    const savedUser = await newUser.save();
+
+    // Sign the token
+    const token = jwt.sign(
+      {
+        user: savedUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    // Send the token in a HTTP-only cookie
+    res
+      .cookie('token', token, {
+        httpOnly: true,
+      })
+      .send();
+
+    console.log(token);
   } catch (error) {
     console.error(error);
     res.status(500).send();
