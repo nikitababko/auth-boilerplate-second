c1c142644c7a6ba172042b148dcc4321f7f9ada6
Nikita Babko <niki-babko@yandex.ru>
2021-02-23 21:59:52 +0700

    Added middleware of auth.

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token)
+      return res.status(401).json({
+        errorMessage: 'Unauthorized',
+      });
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified.user;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({
+      errorMessage: 'Unauthorized',
+    });
+  }
+};
+
+module.exports = auth;
