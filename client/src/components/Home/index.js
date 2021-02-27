33a26755c84484e90b20283310450fed21efa30e
Nikita Babko <niki-babko@yandex.ru>
2021-02-27 19:49:12 +0700

    Added conditions.

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Login, Register, Customers } from '../';
+import AuthContext from 'context/AuthContext';
 import Navbar from '../Layout/Navbar';
 
 const Home = () => {
+  const { loggedIn } = useContext(AuthContext);
+
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/customers" component={Customers} />
+        {loggedIn === false && (
+          <>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </>
+        )}
+        {loggedIn === true && <Route path="/customers" component={Customers} />}
       </Switch>
     </Router>
   );
