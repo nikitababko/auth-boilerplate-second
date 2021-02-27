cfc3586ffa3fbe4614910cfe7cc2b6d873b5a774
Nikita Babko <niki-babko@yandex.ru>
2021-02-27 11:56:21 +0700

    Refactored code.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import axios from 'axios';
+
 import { Home } from './components';
 
+axios.defaults.withCredentials = true;
+
 const App = () => {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <Home />;
 };
 
 export default App;
