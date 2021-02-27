8c95b79cb9b7233d63474c6fb260c5c41cf36801
Nikita Babko <niki-babko@yandex.ru>
2021-02-27 21:29:25 +0700

    Added get.loggedIn.

--- server/src/routers/userRouter.js
+++ server/src/routers/userRouter.js
@@ -124,4 +124,17 @@ router.get('/logout', (req, res) => {
     .send();
 });
 
+router.get('/loggedIn', (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) return res.json(false);
+
+    jwt.verify(token, process.env.JWT_SECRET);
+
+    res.send(true);
+  } catch (error) {
+    res.json(false);
+  }
+});
+
 module.exports = router;
