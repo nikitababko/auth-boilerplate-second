fabc036b86cf3ac5ad38b7787547b1cf048bd122
Nikita Babko <niki-babko@yandex.ru>
2021-02-27 19:48:56 +0700

    Added conditions.

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { LogOutBtn } from 'components';
+import AuthContext from 'context/AuthContext';
+
 const Navbar = () => {
+  const { loggedIn } = useContext(AuthContext);
+
   return (
     <div>
       <Link to="/home">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/customers">Customers</Link>
+      {loggedIn === false && (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </>
+      )}
+
+      {loggedIn === true && (
+        <>
+          <Link to="/customers">Customers</Link>
+          <LogOutBtn />
+        </>
+      )}
     </div>
   );
 };
