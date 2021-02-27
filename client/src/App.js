4523b2f1c723323606f73e5844d72065bd0ede7c
Nikita Babko <niki-babko@yandex.ru>
2021-02-27 19:46:31 +0700

    Added AuthContextProvider.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import axios from 'axios';
 
 import { Home } from './components';
+import { AuthContextProvider } from './context/AuthContext';
 
 axios.defaults.withCredentials = true;
 
 const App = () => {
-  return <Home />;
+  return (
+    <AuthContextProvider>
+      <Home />
+    </AuthContextProvider>
+  );
 };
 
 export default App;
