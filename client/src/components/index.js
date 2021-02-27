c5f01f5d7d6b9a6d85aa547d88f98987c304a4fe
Nikita Babko <niki-babko@yandex.ru>
2021-02-27 19:47:42 +0700

    Imported LogOutBtn, CustomerList, CustomerForm.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,7 @@
 export { default as Home } from './Home';
 export { default as Login } from './auth/Login';
 export { default as Register } from './auth/Register';
+export { default as LogOutBtn } from './auth/LogOutBtn';
 export { default as Customers } from './Customers';
+export { default as CustomerList } from './CustomerList';
+export { default as CustomerForm } from './CustomerForm';
